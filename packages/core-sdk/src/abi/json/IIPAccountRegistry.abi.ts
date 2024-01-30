44b3bf41b28b681634a7efafb0e2736b3425fa2a
Ze Huang <edisonz0718@gmail.com>
2024-01-29 18:52:28 -0800

    Add license abis

--- packages/core-sdk/src/abi/json/IIPAccountRegistry.abi.ts
+++ packages/core-sdk/src/abi/json/IIPAccountRegistry.abi.ts
@@ -1,34 +1,5 @@
 export default [
   {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "chainId_",
-        type: "uint256",
-      },
-      {
-        internalType: "address",
-        name: "tokenContract_",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "tokenId_",
-        type: "uint256",
-      },
-    ],
-    name: "ipAsset",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
     anonymous: false,
     inputs: [
       {
