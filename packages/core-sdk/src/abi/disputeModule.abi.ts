1aaad3f9c4748b4620997c381d542340553cf8df
Don <100505855+DonFungible@users.noreply.github.com>
2024-02-01 16:23:53 -0800

    add abi

---
+++ packages/core-sdk/src/abi/disputeModule.abi.ts
@@ -0,0 +1,12 @@
+import { formatAbi } from "abitype";
+import { getAddress } from "viem";
+
+import DisputeModuleABI from "./json/DisputeModule.abi";
+
+export const DisputeModuleRaw = DisputeModuleABI;
+export const DisputeModuleReadable = formatAbi(DisputeModuleRaw);
+
+export const DisputeModuleConfig = {
+  abi: DisputeModuleRaw,
+  address: getAddress(process.env.TAGGING_MODULE || process.env.NEXT_PUBLIC_TAGGING_MODULE || ""),
+};
