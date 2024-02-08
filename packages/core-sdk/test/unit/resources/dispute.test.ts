57e11d7efc8f91682cd71af266878b34873522f4
Don <100505855+DonFungible@users.noreply.github.com>
2024-02-08 13:36:27 -0500

    add a test

--- packages/core-sdk/test/unit/resources/dispute.test.ts
+++ packages/core-sdk/test/unit/resources/dispute.test.ts
@@ -3,9 +3,10 @@ import { createMock } from "../testUtils";
 import chai from "chai";
 import { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { PublicClient, WalletClient } from "viem";
+import { Address, PublicClient, WalletClient } from "viem";
 import { DisputeClient } from "../../../src/resources/dispute";
 import { AddressZero } from "../../../src";
+import { RaiseDisputeRequest } from "../../../src/types/resources/dispute";
 
 chai.use(chaiAsPromised);
 
@@ -14,32 +15,41 @@ describe("Test DisputeClient", function () {
   let rpcMock: PublicClient;
   let walletMock: WalletClient;
 
+  const mock = {
+    txHash: "0x063834efe214f4199b1ad7181ce8c5ced3e15d271c8e866da7c89e86ee629cfb",
+  };
+
   beforeEach(function () {
     rpcMock = createMock<PublicClient>();
     walletMock = createMock<WalletClient>();
     disputeClient = new DisputeClient(rpcMock, walletMock);
   });
 
-  afterEach(function () {
-    sinon.restore();
-  });
+  describe("[Write Functions] Should be able to", async function () {
+    it("raise a dispute", async () => {
+      rpcMock.readContract = sinon.stub().resolves(AddressZero);
+      rpcMock.simulateContract = sinon.stub().resolves({ request: null });
+      walletMock.writeContract = sinon.stub().resolves(mock.txHash);
 
-  describe("Whitelist Dispute Tags", async function () {
-    it("should be able to whitelist dispute tags", async () => {
-      const mockTxHash = "0xeef10fc5170f669b86c4cd0444882a96087221325f8bf2f55d6188633aa7be7c";
-      rpcMock.simulateContract = sinon.stub().resolves();
-      walletMock.writeContract = sinon.stub().resolves(mockTxHash);
-
-      const response = await disputeClient.whitelistDisputeTags({
-        tag: "testTag",
-        allowed: true,
+      const raiseDisputeRequest = {
+        targetIpId:
+          "0xa3028b46ff4aeba585ebfa1c241ad4a453b6f10dc7bc3d3ebaa9cecc680a6f71" as `0x${string}`,
+        arbitrationPolicy: "0xC6A1c49BCeeE2E512167d5c03e4753776477730b" as `0x${string}`,
+        linkToDisputeEvidence: "foo",
+        targetTag: "bar",
         txOptions: {
-          waitForTransaction: false,
+          waitForTransaction: true,
         },
-      });
+      };
+      const response = await expect(disputeClient.raiseDispute(raiseDisputeRequest)).to.not.be
+        .rejected;
 
       expect(response.txHash).to.be.a("string");
       expect(response.txHash).not.empty;
     });
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
