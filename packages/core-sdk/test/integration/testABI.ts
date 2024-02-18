88b6f0013355b6ae85484a2f48781a1d625b4140
Don <100505855+DonFungible@users.noreply.github.com>
2024-02-18 15:56:28 -0800

    Organize contract addresses (#71)
    * centralize contract addresses
    
    * remove submodule
    
    * move contract addresses

--- packages/core-sdk/test/integration/testABI.ts
+++ packages/core-sdk/test/integration/testABI.ts
@@ -10,60 +10,62 @@ import PILPolicyFrameworkManagerABI from "../../src/abi/json/PILPolicyFrameworkM
 import RegistrationModuleABI from "../../src/abi/json/RegistrationModule.abi";
 import TaggingModuleABI from "../../src/abi/json/TaggingModule.abi";
 import ErrorsABI from "../../src/abi/json/Errors.abi";
-/*
- "AccessController": "0x674d6E1f7b5e2d714DBa588e9d046965225517c8",
-    "ArbitrationPolicySP": "0xb41BC78478878B338336C5E7a34292213779cd6F",
-    "DisputeModule": "0x3A96cad7b2aC783a6811c7c3e8DEF30E8a4cfcDb",
-    "IPAccountImpl": "0x7481a227A11860E80f69AB39d0165258f4c139f6",
-    "IPAccountRegistry": "0x74Cbd8CCc22290FeaaE8421D4FFc6760210B5B0C",
-    "IPAssetRegistry": "0xb1534826Bc9D77d818CbC596435f530778C73865",
-    "LicenseRegistry": "0x66f6865668F2B9213Ed05b97eE97beb97A75e243",
-    "LicensingModule": "0x2ac240293f12032E103458451dE8A8096c5A72E8",
-    "PILPolicyFrameworkManager": "0x3C30b98f56b469c0d292EFF5878e9Fb302CB13dD",
-    "RegistrationModule": "0x193f0Cc84d51Fc38a30658d7eFffEB2C5Cc25840",
-    "TaggingModule": "0x92787d8e2E5fE563A91Ee37B6e953BD0A4bAC1eD"
-*/
+
+export const tenderly = {
+  AccessController: "0x674d6E1f7b5e2d714DBa588e9d046965225517c8",
+  ArbitrationPolicySP: "0xb41BC78478878B338336C5E7a34292213779cd6F",
+  DisputeModule: "0x3A96cad7b2aC783a6811c7c3e8DEF30E8a4cfcDb",
+  IPAccountImpl: "0x7481a227A11860E80f69AB39d0165258f4c139f6",
+  IPAccountRegistry: "0x74Cbd8CCc22290FeaaE8421D4FFc6760210B5B0C",
+  IPAssetRegistry: "0xb1534826Bc9D77d818CbC596435f530778C73865",
+  LicenseRegistry: "0x66f6865668F2B9213Ed05b97eE97beb97A75e243",
+  LicensingModule: "0x2ac240293f12032E103458451dE8A8096c5A72E8",
+  PILPolicyFrameworkManager: "0x3C30b98f56b469c0d292EFF5878e9Fb302CB13dD",
+  RegistrationModule: "0x193f0Cc84d51Fc38a30658d7eFffEB2C5Cc25840",
+  TaggingModule: "0x92787d8e2E5fE563A91Ee37B6e953BD0A4bAC1eD",
+};
+
 export const IPAccountABI = [...IPAccountImplABI, ...ErrorsABI];
 
 export const IPAssetRegistryConfig = {
   abi: IPAssetRegistryABI,
-  address: getAddress("0xb1534826Bc9D77d818CbC596435f530778C73865"),
+  address: getAddress(tenderly.IPAssetRegistry),
 };
 
 export const AccessControllerConfig = {
   abi: [...AccessControllerABI, ...ErrorsABI],
-  address: getAddress("0x674d6E1f7b5e2d714DBa588e9d046965225517c8"),
+  address: getAddress(tenderly.AccessController),
 };
 
 export const DisputeModuleConfig = {
   abi: [...DisputeModuleABI, ...ErrorsABI],
-  address: getAddress("0x3A96cad7b2aC783a6811c7c3e8DEF30E8a4cfcDb"),
+  address: getAddress(tenderly.DisputeModule),
 };
 
 export const LicenseRegistryConfig = {
   abi: IERC1155ABI,
-  address: getAddress("0x66f6865668F2B9213Ed05b97eE97beb97A75e243"),
+  address: getAddress(tenderly.LicenseRegistry),
 };
 
 export const LicensingModuleConfig = {
   abi: LicensingModuleABI,
   //abi: [...LicensingModuleABI, ...ErrorsABI],
-  address: getAddress("0x2ac240293f12032E103458451dE8A8096c5A72E8"),
+  address: getAddress(tenderly.LicensingModule),
 };
 
 export const RegistrationModuleConfig = {
   abi: RegistrationModuleABI,
   //abi: [...RegistrationModuleABI, ...ErrorsABI],
-  address: getAddress("0x193f0Cc84d51Fc38a30658d7eFffEB2C5Cc25840"),
+  address: getAddress(tenderly.RegistrationModule),
 };
 
 export const TaggingModuleConfig = {
   abi: [...TaggingModuleABI, ...ErrorsABI],
-  address: getAddress("0x92787d8e2E5fE563A91Ee37B6e953BD0A4bAC1eD"),
+  address: getAddress(tenderly.TaggingModule),
 };
 
 export const PILPolicyFrameworkManagerConfig = {
   abi: PILPolicyFrameworkManagerABI,
   //abi: [...PILPolicyFrameworkManagerABI, ...ErrorsABI],
-  address: getAddress("0x3C30b98f56b469c0d292EFF5878e9Fb302CB13dD"),
+  address: getAddress(tenderly.PILPolicyFrameworkManager),
 };
