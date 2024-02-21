c8d6b02ee6971bac5c5c6dbe10f6af2033a9e732
Don <100505855+DonFungible@users.noreply.github.com>
2024-02-20 23:02:51 -0800

    clean up comments

--- packages/core-sdk/src/resources/permission.ts
+++ packages/core-sdk/src/resources/permission.ts
@@ -21,11 +21,6 @@ export class PermissionClient {
    * Each policy is represented as a mapping from an IP account address to a signer address to a recipient
    * address to a function selector to a permission level. The permission level can be 0 (ABSTAIN), 1 (ALLOW), or
    * 2 (DENY).
-   * By default, all policies are set to 0 (ABSTAIN), which means that the permission is not set.
-   * The owner of ipAccount by default has all permission.
-   * address(0) => wildcard
-   * bytes4(0) => wildcard
-   * Specific permission overrides wildcard permission.
    * @param request The request object containing necessary data to set permissions.
    *   @param request.ipAsset The address of the IP account that grants the permission for `signer`
    *   @param request.signer The address that can call `to` on behalf of the `ipAccount`
