be30fa0fe634160f346766ed48a4e4e45e4a0c36
Don <100505855+DonFungible@users.noreply.github.com>
2024-02-21 14:45:01 -0800

    update UML to PIL

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,10 +1,10 @@
 // All hooks and generated code are exported from here
 export * from "./generated/registrationModule";
 export * from "./generated/licensingModule";
-export * from "./generated/UMLPolicyFrameworkManager";
+export * from "./generated/PILPolicyFrameworkManager";
 export * from "./generated/disputeModule";
 export * from "./generated/ipAccountImpl";
 export * from "./generated/ipAssetRegistry";
 
 // Rename hooks for better DX
-export { useWriteUmlPolicyFrameworkManagerRegisterPolicy as useRegisterUMLPolicy } from "./generated/UMLPolicyFrameworkManager";
+export { useWritePilPolicyFrameworkManagerRegisterPolicy as useRegisterPILPolicy } from "./generated/PILPolicyFrameworkManager";
