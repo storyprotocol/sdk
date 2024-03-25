6b7cc0ac0c6bc946c8fcdb1ca8df99dd02489c73
bonnie <bonnie@57blocks.com>
2024-03-25 14:43:42 +0800

    Add renaissance chain

---
+++ packages/core-sdk/test/integration/renaissance/config/testABI.renaissance.ts
@@ -0,0 +1,49 @@
+import { getAddress } from "viem";
+
+import IERC1155ABI from "../../../../src/abi/json/IERC1155.abi";
+import AccessControllerABI from "../../../../src/abi/json/AccessController.abi";
+import DisputeModuleABI from "../../../../src/abi/json/DisputeModule.abi";
+import IPAccountImplABI from "../../../../src/abi/json/IPAccountImpl.abi";
+import IPAssetRegistryABI from "../../../../src/abi/json/IIPAssetRegistry.abi";
+import LicensingModuleABI from "../../../../src/abi/json/LicensingModule.abi";
+import PILPolicyFrameworkManagerABI from "../../../../src/abi/json/PILPolicyFrameworkManager.abi";
+import RegistrationModuleABI from "../../../../src/abi/json/RegistrationModule.abi";
+import ErrorsABI from "../../../../src/abi/json/Errors.abi";
+import renaissance from "./renaissance.env";
+//TODO: make chain address dynamic confirmed with Ze
+export const IPAccountABI = [...IPAccountImplABI, ...ErrorsABI];
+
+export const IPAssetRegistryConfig = {
+  abi: IPAssetRegistryABI,
+  address: getAddress(renaissance.IPAssetRegistry),
+};
+
+export const AccessControllerConfig = {
+  abi: [...AccessControllerABI, ...ErrorsABI],
+  address: getAddress(renaissance.AccessController),
+};
+
+export const DisputeModuleConfig = {
+  abi: [...DisputeModuleABI, ...ErrorsABI],
+  address: getAddress(renaissance.DisputeModule),
+};
+
+export const LicenseRegistryConfig = {
+  abi: IERC1155ABI,
+  address: getAddress(renaissance.LicenseRegistry),
+};
+
+export const LicensingModuleConfig = {
+  abi: [...LicensingModuleABI, ...ErrorsABI],
+  address: getAddress(renaissance.LicensingModule),
+};
+
+export const RegistrationModuleConfig = {
+  abi: [...RegistrationModuleABI, ...ErrorsABI],
+  address: getAddress(renaissance.RegistrationModule),
+};
+
+export const PILPolicyFrameworkManagerConfig = {
+  abi: [...PILPolicyFrameworkManagerABI, ...ErrorsABI],
+  address: getAddress(renaissance.PILPolicyFrameworkManager),
+};
