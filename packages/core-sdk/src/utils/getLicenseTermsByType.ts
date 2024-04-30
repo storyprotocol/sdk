590ea24f9feeb6dec5b24d0c3049285ee0361f01
bonnie <bonnie@57blocks.com>
2024-04-30 16:15:20 +0800

    Add unit tests about getLicenseTermByType

--- packages/core-sdk/src/utils/getLicenseTermsByType.ts
+++ packages/core-sdk/src/utils/getLicenseTermsByType.ts
@@ -1,4 +1,4 @@
-import { Hex, zeroAddress } from "viem";
+import { Hex, getAddress, zeroAddress } from "viem";
 
 import { PIL_TYPE, LicenseTerms } from "../types/resources/license";
 
@@ -33,21 +33,34 @@ export function getLicenseTermByType(
   if (type === PIL_TYPE.NON_COMMERCIAL_REMIX) {
     return licenseTerms;
   } else if (type === PIL_TYPE.COMMERCIAL_USE) {
-    licenseTerms.royaltyPolicy = term!.royaltyPolicyLAPAddress;
-    licenseTerms.mintingFee = BigInt(term!.mintingFee);
+    if (!term || term.mintingFee === undefined || term.currency === undefined) {
+      throw new Error("mintingFee currency are required for commercial use PIL.");
+    }
+    licenseTerms.royaltyPolicy = getAddress(term.royaltyPolicyLAPAddress);
+    licenseTerms.mintingFee = BigInt(term.mintingFee);
     licenseTerms.commercialUse = true;
     licenseTerms.commercialAttribution = true;
     licenseTerms.derivativesReciprocal = false;
-    licenseTerms.currency = term!.currency;
+    licenseTerms.currency = getAddress(term.currency);
     return licenseTerms;
   } else {
-    licenseTerms.royaltyPolicy = term!.royaltyPolicyLAPAddress;
-    licenseTerms.mintingFee = BigInt(term!.mintingFee);
+    if (
+      !term ||
+      term.mintingFee === undefined ||
+      term.currency === undefined ||
+      term.commercialRevShare === undefined
+    ) {
+      throw new Error(
+        "mintingFee, currency and commercialRevShare are required for commercial remix PIL.",
+      );
+    }
+    licenseTerms.royaltyPolicy = getAddress(term.royaltyPolicyLAPAddress);
+    licenseTerms.mintingFee = BigInt(term.mintingFee!);
     licenseTerms.commercialUse = true;
     licenseTerms.commercialAttribution = true;
-    licenseTerms.commercialRevShare = term!.commercialRevShare!;
+    licenseTerms.commercialRevShare = term.commercialRevShare;
     licenseTerms.derivativesReciprocal = true;
-    licenseTerms.currency = term!.currency;
+    licenseTerms.currency = getAddress(term.currency);
     return licenseTerms;
   }
 }
