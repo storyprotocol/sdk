06282385573101c80791c6428c2dc17b88774c22
bonnie <bonnie@57blocks.com>
2024-05-14 15:45:10 +0800

    Complete dispute integration tests

--- packages/core-sdk/test/integration/dispute.test.ts
+++ packages/core-sdk/test/integration/dispute.test.ts
@@ -1,79 +1,139 @@
 import chai from "chai";
 import { StoryClient } from "../../src";
+import { CancelDisputeRequest, RaiseDisputeRequest, ResolveDisputeRequest } from "../../src/index";
 import {
-  CancelDisputeRequest,
-  RaiseDisputeRequest,
-  ResolveDisputeRequest,
-} from "../../src/types/resources/dispute";
-import { MockERC721, getStoryClientInSepolia, getTokenId } from "./utils/util";
+  MockERC721,
+  getStoryClientInSepolia,
+  getTokenId,
+  publicClient,
+  sepoliaChainId,
+  walletClient,
+} from "./utils/util";
 import chaiAsPromised from "chai-as-promised";
+import { Address, Hex } from "viem";
+import { MockERC20 } from "./utils/mockERC20";
+import { privateKeyToAccount } from "viem/accounts";
+import { disputeModuleAddress } from "../../src/abi/generated";
+
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe.skip("Dispute Functions", () => {
-  let client: StoryClient;
+const arbitrationPolicyAddress = "0xc07Bc791CF55E718BA7D70cE650B3152BbE3325e";
+describe("Dispute Functions", () => {
+  let clientA: StoryClient;
+  let clientB: StoryClient;
+  let disputeId: number;
+  let ipIdB: Address;
+  async function setDisputeJudgement(
+    WALLET_PRIVATE_KEY: Hex,
+    disputeId: bigint,
+    decision: boolean,
+    data: Hex,
+  ) {
+    try {
+      const account = privateKeyToAccount(WALLET_PRIVATE_KEY);
+      const contractAbi = {
+        inputs: [
+          { internalType: "uint256", name: "disputeId", type: "uint256" },
+          { internalType: "bool", name: "decision", type: "bool" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        name: "setDisputeJudgement",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      };
 
-  before(function () {
-    client = getStoryClientInSepolia();
-  });
+      const requestArgs = {
+        address: disputeModuleAddress[sepoliaChainId],
+        functionName: "setDisputeJudgement",
+        args: [disputeId, decision, data],
+        abi: [contractAbi],
+        account: account,
+      };
 
-  describe("Should be able to", async function () {
-    it("raise a dispute", async () => {
-      const tokenId = await getTokenId();
-      const registerResult = await client.ipAsset.register({
+      await publicClient.simulateContract(requestArgs);
+      const hash = await walletClient.writeContract(requestArgs);
+      await publicClient.waitForTransactionReceipt({
+        hash: hash,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  before(async function () {
+    clientA = getStoryClientInSepolia();
+    clientB = getStoryClientInSepolia(process.env.SEPOLIA_WALLET_PRIVATE_KEY2 as Address);
+    const mockERC20 = new MockERC20();
+    await mockERC20.approve(arbitrationPolicyAddress);
+    const tokenId = await getTokenId();
+    ipIdB = (
+      await clientB.ipAsset.register({
         nftContract: MockERC721,
         tokenId: tokenId!,
         txOptions: {
           waitForTransaction: true,
         },
-      });
-      const raiseDisputeRequest: RaiseDisputeRequest = {
-        targetIpId: registerResult.ipId!,
-        arbitrationPolicy: "0xb41BC78478878B338336C5E7a34292213779cd6F",
-        linkToDisputeEvidence: "foo",
-        targetTag: "PLAGIARISM",
-        txOptions: {
-          waitForTransaction: true,
-        },
-      };
-      const response = await expect(client.dispute.raiseDispute(raiseDisputeRequest)).to.not.be
-        .rejected;
+      })
+    ).ipId!;
+  });
 
-      expect(response.txHash).to.be.a("string");
-      expect(response.txHash).not.empty;
+  it("raise a dispute", async () => {
+    const raiseDisputeRequest: RaiseDisputeRequest = {
+      targetIpId: ipIdB,
+      arbitrationPolicy: arbitrationPolicyAddress,
+      linkToDisputeEvidence: "foo",
+      targetTag: "PLAGIARISM",
+      txOptions: {
+        waitForTransaction: true,
+      },
+    };
+    const response = await expect(clientA.dispute.raiseDispute(raiseDisputeRequest)).to.not.be
+      .rejected;
+    disputeId = response.disputeId;
+    expect(response.txHash).to.be.a("string").and.not.empty;
+    expect(response.disputeId).to.be.a("bigint");
+  });
 
-      if (true) {
-        expect(response.disputeId).to.be.a("string");
-        expect(response.disputeId).not.empty;
-      }
-    });
+  it("cancel a dispute", async () => {
+    const cancelDispute: CancelDisputeRequest = {
+      disputeId: disputeId,
+      txOptions: {
+        waitForTransaction: true,
+      },
+    };
+    const response = await expect(clientA.dispute.cancelDispute(cancelDispute)).to.not.be.rejected;
 
-    it.skip("resolve a dispute", async () => {
-      const resolveDisputeRequest: ResolveDisputeRequest = {
-        disputeId: 3,
-        data: "0x",
-        txOptions: {
-          waitForTransaction: true,
-        },
-      };
-      const response = await expect(client.dispute.resolveDispute(resolveDisputeRequest)).to.not.be
-        .rejected;
+    expect(response.txHash).to.be.a("string").and.not.empty;
+  });
 
-      expect(response.txHash).to.be.a("string");
-      expect(response.txHash).not.empty;
-    });
+  it("resolve a dispute", async () => {
+    const raiseDisputeRequest: RaiseDisputeRequest = {
+      targetIpId: ipIdB,
+      arbitrationPolicy: arbitrationPolicyAddress,
+      linkToDisputeEvidence: "foo",
+      targetTag: "PLAGIARISM",
+      txOptions: {
+        waitForTransaction: true,
+      },
+    };
+    const otherDisputeId = (await clientA.dispute.raiseDispute(raiseDisputeRequest)).disputeId!;
+    await setDisputeJudgement(
+      process.env.SEPOLIA_WALLET_PRIVATE_KEY3 as Hex,
+      otherDisputeId,
+      true,
+      "0x",
+    );
+    const resolveDisputeRequest: ResolveDisputeRequest = {
+      disputeId: otherDisputeId!,
+      data: "0x",
+      txOptions: {
+        waitForTransaction: true,
+      },
+    };
+    const response = await expect(clientA.dispute.resolveDispute(resolveDisputeRequest)).to.not.be
+      .rejected;
 
-    it("cancel a dispute", async () => {
-      const cancelDispute: CancelDisputeRequest = {
-        disputeId: 3,
-        txOptions: {
-          waitForTransaction: true,
-        },
-      };
-      const response = await expect(client.dispute.cancelDispute(cancelDispute)).to.not.be.rejected;
-
-      expect(response.txHash).to.be.a("string");
-      expect(response.txHash).not.empty;
-    });
+    expect(response.txHash).to.be.a("string").and.not.empty;
   });
 });
