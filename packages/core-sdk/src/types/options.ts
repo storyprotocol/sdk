85ee7550e218e47b819fb203206b6e8f46b80db2
Bonnie <bonnie@57blocks.com>
2024-09-12 13:34:02 +0800

    Remove useless code

--- packages/core-sdk/src/types/options.ts
+++ packages/core-sdk/src/types/options.ts
@@ -9,23 +9,6 @@ export interface TxOptions extends Omit<WaitForTransactionReceiptParameters, "ha
   // not submit and execute, it will only encode the abi and
   // function data and return.
   encodedTxDataOnly?: boolean;
-  // // The number of confirmations (blocks that have passed) to wait before resolving.
-  // confirmations?: number;
-  // // Optional callback to emit if the transaction has been replaced.
-  // onReplaced?: (object: {
-  //   reason: "replaced" | "repriced" | "cancelled";
-  //   replacedTransaction: Transaction;
-  //   transaction: Transaction;
-  //   transactionReceipt: TransactionReceipt;
-  // }) => void;
-  // // Polling frequency (in ms). Defaults to the Viem Client's pollingInterval config.
-  // pollingInterval?: number;
-  // // Number of times to retry if the transaction or block is not found.
-  // retryCount?: number;
-  // // Time to wait (in ms) between retries.
-  // retryDelay?: number;
-  // // set timeout for waitForTransaction
-  // timeout?: number;
 }
 
 export type WithTxOptions<T> = T & {
