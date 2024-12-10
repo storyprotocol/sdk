f116aa023a1aef04e8849988bd2f65561206020b
bpolania <boris.polania@gmail.com>
2024-12-10 13:23:38 -0800

    Update royalty.test.ts

--- packages/core-sdk/test/integration/royalty.test.ts
+++ packages/core-sdk/test/integration/royalty.test.ts
@@ -112,6 +112,7 @@ describe("Test royalty Functions", () => {
       });
       await transferToken();
     });
+
     it("should not throw error when pay royalty on behalf", async () => {
       const response = await client.royalty.payRoyaltyOnBehalf({
         receiverIpId: parentIpId,
