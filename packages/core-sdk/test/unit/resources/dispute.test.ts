52ee2718142a075c32bd01bb47848bfac4403846
bpolania <boris.polania@gmail.com>
2024-12-10 13:22:12 -0800

    Update dispute.test.ts

--- packages/core-sdk/test/unit/resources/dispute.test.ts
+++ packages/core-sdk/test/unit/resources/dispute.test.ts
@@ -24,11 +24,6 @@ describe("Test DisputeClient", () => {
   });
 
   describe("Test raiseDispute", () => {
-    it("should have a private key", () => {
-      if (!process.env.WALLET_PRIVATE_KEY) {
-        throw new Error("WALLET_PRIVATE_KEY environment variable is not set.");
-      }
-    });
     it("throw address error when call raiseDispute with invalid targetIpId", async () => {
       try {
         await disputeClient.raiseDispute({
