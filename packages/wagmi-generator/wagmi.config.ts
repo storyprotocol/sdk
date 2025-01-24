def9c116a4fa18c4456c6e135a9455ffa5591a24
Bonnie <bonnie@57blocks.com>
2025-01-24 19:09:01 +0800

    Update wagmi about homer

--- packages/wagmi-generator/wagmi.config.ts
+++ packages/wagmi-generator/wagmi.config.ts
@@ -4,7 +4,7 @@ import type { Evaluate } from "@wagmi/cli/src/types";
 import type { ContractConfig } from "@wagmi/cli/src/config";
 import { resolveProxyContracts } from "./resolveProxyContracts";
 import { optimizedBlockExplorer } from "./optimizedBlockExplorer";
-const devnetChainId = 1315;
+const homerChainId = 1315;
 import "dotenv/config";
 
 export default defineConfig(async () => {
@@ -12,169 +12,169 @@ export default defineConfig(async () => {
     {
       name: "AccessController",
       address: {
-        [devnetChainId]: "0xcCF37d0a503Ee1D4C11208672e622ed3DFB2275a",
+        [homerChainId]: "0xcCF37d0a503Ee1D4C11208672e622ed3DFB2275a",
       },
     },
     {
       name: "DisputeModule",
       address: {
-        [devnetChainId]: "0x9b7A9c70AFF961C799110954fc06F3093aeb94C5",
+        [homerChainId]: "0x9b7A9c70AFF961C799110954fc06F3093aeb94C5",
       },
     },
     {
       name: "IPAccountImpl",
       address: {
-        [devnetChainId]: "0x7343646585443F1c3F64E4F08b708788527e1C77",
+        [homerChainId]: "0x7343646585443F1c3F64E4F08b708788527e1C77",
       },
     },
     {
       name: "IPAssetRegistry",
       address: {
-        [devnetChainId]: "0x77319B4031e6eF1250907aa00018B8B1c67a244b",
+        [homerChainId]: "0x77319B4031e6eF1250907aa00018B8B1c67a244b",
       },
     },
     {
       name: "IpRoyaltyVaultImpl",
       address: {
-        [devnetChainId]: "0x63cC7611316880213f3A4Ba9bD72b0EaA2010298",
+        [homerChainId]: "0x63cC7611316880213f3A4Ba9bD72b0EaA2010298",
       },
     },
     {
       name: "LicenseRegistry",
       address: {
-        [devnetChainId]: "0x529a750E02d8E2f15649c13D69a465286a780e24",
+        [homerChainId]: "0x529a750E02d8E2f15649c13D69a465286a780e24",
       },
     },
     {
       name: "LicenseToken",
       address: {
-        [devnetChainId]: "0xFe3838BFb30B34170F00030B52eA4893d8aAC6bC",
+        [homerChainId]: "0xFe3838BFb30B34170F00030B52eA4893d8aAC6bC",
       },
     },
     {
       name: "LicensingModule",
       address: {
-        [devnetChainId]: "0x04fbd8a2e56dd85CFD5500A4A4DfA955B9f1dE6f",
+        [homerChainId]: "0x04fbd8a2e56dd85CFD5500A4A4DfA955B9f1dE6f",
       },
     },
     {
       name: "PILicenseTemplate",
       address: {
-        [devnetChainId]: "0x2E896b0b2Fdb7457499B56AAaA4AE55BCB4Cd316",
+        [homerChainId]: "0x2E896b0b2Fdb7457499B56AAaA4AE55BCB4Cd316",
       },
     },
     {
       name: "ModuleRegistry",
       address: {
-        [devnetChainId]: "0x022DBAAeA5D8fB31a0Ad793335e39Ced5D631fa5",
+        [homerChainId]: "0x022DBAAeA5D8fB31a0Ad793335e39Ced5D631fa5",
       },
     },
     {
       name: "RoyaltyModule",
       address: {
-        [devnetChainId]: "0xD2f60c40fEbccf6311f8B47c4f2Ec6b040400086",
+        [homerChainId]: "0xD2f60c40fEbccf6311f8B47c4f2Ec6b040400086",
       },
     },
     {
       name: "RoyaltyPolicyLAP",
       address: {
-        [devnetChainId]: "0xBe54FB168b3c982b7AaE60dB6CF75Bd8447b390E",
+        [homerChainId]: "0xBe54FB168b3c982b7AaE60dB6CF75Bd8447b390E",
       },
     },
     {
       name: "ArbitrationPolicyUMA",
       address: {
-        [devnetChainId]: "0xfFD98c3877B8789124f02C7E8239A4b0Ef11E936",
+        [homerChainId]: "0xfFD98c3877B8789124f02C7E8239A4b0Ef11E936",
       },
     },
     {
       name: "RoyaltyPolicyLRP",
       address: {
-        [devnetChainId]: "0x9156e603C949481883B1d3355c6f1132D191fC41",
+        [homerChainId]: "0x9156e603C949481883B1d3355c6f1132D191fC41",
       },
     },
     {
       name: "SPGNFTBeacon",
       address: {
-        [devnetChainId]: "0xD2926B9ecaE85fF59B6FB0ff02f568a680c01218",
+        [homerChainId]: "0xD2926B9ecaE85fF59B6FB0ff02f568a680c01218",
       },
     },
     {
       name: "SPGNFTImpl",
       address: {
-        [devnetChainId]: "0x6Cfa03Bc64B1a76206d0Ea10baDed31D520449F5",
+        [homerChainId]: "0x6Cfa03Bc64B1a76206d0Ea10baDed31D520449F5",
       },
     },
     {
       name: "CoreMetadataModule",
       address: {
-        [devnetChainId]: "0x6E81a25C99C6e8430aeC7353325EB138aFE5DC16",
+        [homerChainId]: "0x6E81a25C99C6e8430aeC7353325EB138aFE5DC16",
       },
     },
     {
       name: "DerivativeWorkflows",
       address: {
-        [devnetChainId]: "0x9e2d496f72C547C2C535B167e06ED8729B374a4f",
+        [homerChainId]: "0x9e2d496f72C547C2C535B167e06ED8729B374a4f",
       },
     },
     {
       name: "GroupingWorkflows",
       address: {
-        [devnetChainId]: "0xD7c0beb3aa4DCD4723465f1ecAd045676c24CDCd",
+        [homerChainId]: "0xD7c0beb3aa4DCD4723465f1ecAd045676c24CDCd",
       },
     },
     {
       name: "RegistrationWorkflows",
       address: {
-        [devnetChainId]: "0xbe39E1C756e921BD25DF86e7AAa31106d1eb0424",
+        [homerChainId]: "0xbe39E1C756e921BD25DF86e7AAa31106d1eb0424",
       },
     },
     {
       name: "RoyaltyWorkflows",
       address: {
-        [devnetChainId]: "0x9515faE61E0c0447C6AC6dEe5628A2097aFE1890",
+        [homerChainId]: "0x9515faE61E0c0447C6AC6dEe5628A2097aFE1890",
       },
     },
     {
       name: "LicenseAttachmentWorkflows",
       address: {
-        [devnetChainId]: "0xcC2E862bCee5B6036Db0de6E06Ae87e524a79fd8",
+        [homerChainId]: "0xcC2E862bCee5B6036Db0de6E06Ae87e524a79fd8",
       },
     },
     {
       name: "RoyaltyTokenDistributionWorkflows",
       address: {
-        [devnetChainId]: "0xa38f42B8d33809917f23997B8423054aAB97322C",
+        [homerChainId]: "0xa38f42B8d33809917f23997B8423054aAB97322C",
       },
     },
     {
       name: "GroupingModule",
       address: {
-        [devnetChainId]: "0x69D3a7aa9edb72Bc226E745A7cCdd50D947b69Ac",
+        [homerChainId]: "0x69D3a7aa9edb72Bc226E745A7cCdd50D947b69Ac",
       },
     },
     {
       name: "EvenSplitGroupPool",
       address: {
-        [devnetChainId]: "0xf96f2c30b41Cb6e0290de43C8528ae83d4f33F89",
+        [homerChainId]: "0xf96f2c30b41Cb6e0290de43C8528ae83d4f33F89",
       },
     },
     {
       name: "MockERC20",
       address: {
-        [devnetChainId]: "0xF2104833d386a2734a4eB3B8ad6FC6812F29E38E",
+        [homerChainId]: "0xF2104833d386a2734a4eB3B8ad6FC6812F29E38E",
       },
     },
     {
       name: "ERC20Token",
       address: {
-        [devnetChainId]: "0x1514000000000000000000000000000000000000",
+        [homerChainId]: "0x1514000000000000000000000000000000000000",
       },
     },
     {
       name: "Multicall3",
       address: {
-        [devnetChainId]: "0xca11bde05977b3631167028862be2a173976ca11",
+        [homerChainId]: "0xca11bde05977b3631167028862be2a173976ca11",
       },
     },
   ];
@@ -184,11 +184,11 @@ export default defineConfig(async () => {
     plugins: [
       optimizedBlockExplorer({
         baseUrl: "https://devnet.storyscan.xyz/api",
-        name: "devnet",
+        name: "homer",
         getAddress: await resolveProxyContracts({
           baseUrl: "https://devnet.storyrpc.io",
           contracts: contracts,
-          chainId: devnetChainId,
+          chainId: homerChainId,
         }),
         contracts: contracts,
       }),
