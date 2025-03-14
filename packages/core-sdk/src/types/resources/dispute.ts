94fcf39f676f659baad1730644bb1e678aa118f9
Draco <draco@dracoli.com>
2025-03-14 11:34:24 -0400

    fix: data type for ResolveDisputeRequest

--- packages/core-sdk/src/types/resources/dispute.ts
+++ packages/core-sdk/src/types/resources/dispute.ts
@@ -53,8 +53,11 @@ export type CancelDisputeResponse = {
 
 export type ResolveDisputeRequest = {
   disputeId: number | string | bigint;
-  /** The data to resolve the dispute. */
-  data: Address;
+  /**
+   * The data to resolve the dispute.
+   * Use `"0x"` if no additional data is needed.
+   */
+  data: Hex;
   txOptions?: TxOptions;
 };
 
