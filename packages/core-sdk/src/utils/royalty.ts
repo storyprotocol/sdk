caad2ce5495505b5205481f5e8ecd0e91b5c95bf
Draco <draco@dracoli.com>
2025-03-18 15:12:25 -0400

    add address validation to default case

--- packages/core-sdk/src/utils/royalty.ts
+++ packages/core-sdk/src/utils/royalty.ts
@@ -9,14 +9,12 @@ export const royaltyPolicyInputToAddress = (
   input: RoyaltyPolicyInput,
   chainId: ChainIds,
 ): Address => {
-  // if input is string, validate and return address
-  if (typeof input === "string") {
-    return validateAddress(input);
-  }
   switch (input) {
     case NativeRoyaltyPolicy.LAP:
       return royaltyPolicyLapAddress[chain[chainId]];
     case NativeRoyaltyPolicy.LRP:
       return royaltyPolicyLrpAddress[chain[chainId]];
+    default:
+      return validateAddress(input);
   }
 };
