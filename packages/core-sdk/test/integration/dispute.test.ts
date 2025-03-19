430868e06a939662ea09847c737c20390e5a0e78
Draco <draco@dracoli.com>
2025-03-19 13:27:13 -0400

    fix: revert changes in #480

--- packages/core-sdk/test/integration/dispute.test.ts
+++ packages/core-sdk/test/integration/dispute.test.ts
@@ -168,7 +168,7 @@ describe("Dispute Functions", () => {
       expect(ret.txHash).to.be.a("string").and.not.empty;
 
       // should throw error if attempting to dispute assertion again
-      const secondDispute = clientB.dispute.disputeAssertion({
+      const secondDispute = await clientB.dispute.disputeAssertion({
         ipId: ipIdB,
         assertionId,
         counterEvidenceCID,
@@ -176,7 +176,7 @@ describe("Dispute Functions", () => {
           waitForTransaction: true,
         },
       });
-      await expect(secondDispute).to.be.rejectedWith("Execution reverted for an unknown reason");
+      expect(secondDispute.receipt?.status).to.equal("reverted");
     });
 
     it("should throw error when liveness is out of bounds", async () => {
