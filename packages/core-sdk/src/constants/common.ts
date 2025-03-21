df01012ea010ecf01910a6bfcda18c7e7996ac56
zz <2418184580@qq.com>
2025-03-21 10:38:57 +0800

    perf: use `zeroAddress` instead of `AddressZero` & remove `HashZero` (#415)

--- packages/core-sdk/src/constants/common.ts
+++ packages/core-sdk/src/constants/common.ts
@@ -1,7 +1,5 @@
 import { Hex } from "viem";
 
-export const AddressZero = "0x0000000000000000000000000000000000000000";
-export const HashZero = "0x0000000000000000000000000000000000000000000000000000000000000000";
 export const defaultFunctionSelector: Hex = "0x00000000";
 export const royaltySharesTotalSupply: number = 100_000_000;
 export const MAX_ROYALTY_TOKEN = 100_000_000;
