b986ff2b51ee964df301f0a0d84775198ba309f2
Bonnie <bonnie@57blocks.com>
2025-05-12 11:22:27 +0800

    Upgrade eslint to 9.26.0

---
+++ packages/core-sdk/eslint.config.mjs
@@ -0,0 +1,17 @@
+import config from "@story-protocol/eslint-config";
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...config,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json", "./tsconfig.test.json"],
+      },
+    },
+  },
+  //TODO: need to fix `e` which is not being used in generated files
+  {
+    ignores: ["./src/abi/generated.ts"],
+  },
+];
