bddd5a60d983d5e6809aa8ef32c5ca92355313ce
Bonnie <bonnie@57blocks.com>
2025-06-04 15:07:19 +0800

    Track test check

--- packages/core-sdk/eslint.config.mjs
+++ packages/core-sdk/eslint.config.mjs
@@ -5,6 +5,6 @@ export default [
   ...config,
   //TODO: need to fix `e` which is not being used in generated files
   {
-    ignores: ["./src/abi/generated.ts", "**/test/**/*.ts"],
+    ignores: ["./src/abi/generated.ts"],
   },
 ];
